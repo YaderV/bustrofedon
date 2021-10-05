7cf70a694c4e3f537df668876c4996c419c2fa3e
Yader Velasquez <yajosev@gmail.com>
2021-10-05 00:10:27 -0600

    Generate json model

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -19,7 +21,7 @@ func main() {
 	file, err := os.Open("./example.txt")
 	check(err)
 	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
+	//scanner.Split(bufio.ScanLines)
 	var text []string
 	for scanner.Scan() {
 		s := scanner.Text()
@@ -34,6 +36,30 @@ func main() {
 
 	wholeText := strings.Join(text, "\n")
 	haikus := strings.Split(wholeText, "\n\n")
-	fmt.Printf("%d Haikus Found", len(haikus))
+	haikus_length := len(haikus)
+	fmt.Printf("%d Haikus Found\n", haikus_length)
 
+	var haiku_output = make([]map[string]string, haikus_length)
+
+	for index, text := range haikus {
+		verses := strings.Split(text, "\n")
+
+		var buckets [3]string
+
+		for i, verse := range verses {
+			verseWords := strings.Split(verse, " ")
+			if len(verseWords) > 0 {
+				buckets[i] = strings.Join(verseWords, " ")
+			}
+		}
+		haiku_output[index] = map[string]string{
+			"a": buckets[0],
+			"b": buckets[1],
+			"c": buckets[2],
+		}
+	}
+	jsonString, err := json.MarshalIndent(haiku_output, "", " ")
+	check(err)
+	fmt.Printf("verses: %s\n", jsonString)
+	_ = ioutil.WriteFile("verses.json", jsonString, 0644)
 }
